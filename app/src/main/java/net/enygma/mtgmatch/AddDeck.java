a222f0b797bfd78ba7d3d7adc51e8a3a7d42764a
Em D <nullcoalesence@gmail.com>
2016-07-12 19:04:52 -0700

    Added basic layout to activity pages. Not far planned out, form controls are going to change

---
+++ app/src/main/java/net/enygma/mtgmatch/AddDeck.java
@@ -0,0 +1,13 @@
+package net.enygma.mtgmatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddDeck extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_deck);
+    }
+}
---
+++ app/src/main/java/net/enygma/mtgmatch/FightMatch.java
@@ -0,0 +1,13 @@
+package net.enygma.mtgmatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FightMatch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fight_match);
+    }
+}
